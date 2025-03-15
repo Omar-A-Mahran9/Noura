18a18972adda554bd44ec26f2c223451a7d2f191
Omar-A-Mahran9 <omar.a.m.mahran@gmail.com>
2025-03-15 20:25:09 +0200

    firat commit

--- app/Http/Resources/Api/QuizResource.php
+++ app/Http/Resources/Api/QuizResource.php
@@ -9,20 +9,37 @@ class QuizResource extends JsonResource
 
     public function toArray($request)
     {
-        dd($request);
-        return [
-            'id' => $this->id,
-            'title' => $this->nem,
-            'description' => $this->description,
-            'steps' => [
-                'step_1' => QuizQuestionResource::collection($this->questions->take(5)),
-                'step_2' => QuizQuestionResource::collection($this->questions->skip(5)->take(3)),
-                'step_3' => QuizQuestionResource::collection($this->questions->skip(8)->take(6)), // Remaining questions
-                'step_4' => QuizQuestionResource::collection($this->questions->skip(14)->take(1)), // Remaining questions
-                'step_5' => QuizQuestionResource::collection($this->questions->skip(15)->take(1)), // Remaining questions
-                'step_6' => QuizQuestionResource::collection($this->questions->skip(16)->take(4)), // Remaining questions
-                'step_7' => QuizQuestionResource::collection($this->questions->skip(20)->take(1)), // Remaining questions
-                'step_8' => QuizQuestionResource::collection($this->questions->skip(21)->take(4)), // Remaining questions
 
-            ],        ];    }
+            // Get the consultaion_type_id from the request
+            $consultaionTypeId = $request->input('consultaion_type_id');
+
+            // Apply different logic if consultaion_type_id == 1
+            if ($consultaionTypeId == 1) {
+                return [
+                    'id' => $this->id,
+                    'title' => $this->name,
+                    'description' => $this->description,
+                    'steps' => [
+                        'step_1' => QuizQuestionResource::collection($this->questions->take(5)),
+                        'step_2' => QuizQuestionResource::collection($this->questions->skip(5)->take(3)),
+                        'step_3' => QuizQuestionResource::collection($this->questions->skip(8)->take(6)),
+                        'step_4' => QuizQuestionResource::collection($this->questions->skip(14)->take(1)),
+                        'step_5' => QuizQuestionResource::collection($this->questions->skip(15)->take(1)),
+                        'step_6' => QuizQuestionResource::collection($this->questions->skip(16)->take(4)),
+                        'step_7' => QuizQuestionResource::collection($this->questions->skip(20)->take(1)),
+                        'step_8' => QuizQuestionResource::collection($this->questions->skip(21)->take(4)),
+                    ],
+                ];
+            }
+
+            return [
+                'id' => $this->id,
+                'title' => $this->name,
+                'description' => $this->description,
+                'questions' => QuizQuestionResource::collection($this->questions),
+            ];
+            // Default response
+
+
+    }
 }
