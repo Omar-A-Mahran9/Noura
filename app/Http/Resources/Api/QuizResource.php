6d195e0d9703325243571e6bec86fdb492ed6160
Omar-A-Mahran9 <omar.a.m.mahran@gmail.com>
2025-03-15 20:20:06 +0200

    firat commit

--- app/Http/Resources/Api/QuizResource.php
+++ app/Http/Resources/Api/QuizResource.php
@@ -16,6 +16,12 @@ class QuizResource extends JsonResource
             'steps' => [
                 'step_1' => QuizQuestionResource::collection($this->questions->take(5)),
                 'step_2' => QuizQuestionResource::collection($this->questions->skip(5)->take(3)),
-                'step_3' => QuizQuestionResource::collection($this->questions->skip(8)), // Remaining questions
+                'step_3' => QuizQuestionResource::collection($this->questions->skip(8)->take(6)), // Remaining questions
+                'step_4' => QuizQuestionResource::collection($this->questions->skip(14)->take(1)), // Remaining questions
+                'step_5' => QuizQuestionResource::collection($this->questions->skip(15)->take(1)), // Remaining questions
+                'step_6' => QuizQuestionResource::collection($this->questions->skip(16)->take(4)), // Remaining questions
+                'step_7' => QuizQuestionResource::collection($this->questions->skip(20)->take(1)), // Remaining questions
+                'step_8' => QuizQuestionResource::collection($this->questions->skip(21)->take(4)), // Remaining questions
+
             ],        ];    }
 }
