3dca02aee6d0c8f7c38f4edbe68e30b2e4eda5dc
Omar-A-Mahran9 <omar.a.m.mahran@gmail.com>
2025-03-16 04:25:21 +0200

    firat commit

--- app/Http/Resources/Api/QuizResource.php
+++ app/Http/Resources/Api/QuizResource.php
@@ -39,14 +39,26 @@ $consultaionTypeId = (int) $request->input('consultaion_type_id');
                     'title' => $this->name,
                     'description' => $this->description,
                     'steps' => [
-                        'step_1' => QuizQuestionResource::collection($this->questions->take(5)),
-                        'step_2' => QuizQuestionResource::collection($this->questions->skip(5)->take(3)),
-                        'step_3' => QuizQuestionResource::collection($this->questions->skip(8)->take(6)),
-                        'step_4' => QuizQuestionResource::collection($this->questions->skip(14)->take(1)),
+                        'step_1' => QuizQuestionResource::collection($this->questions->take(7)),
+                        'step_2' => QuizQuestionResource::collection($this->questions->skip(7)->take(1)),
+                        'step_3' => QuizQuestionResource::collection($this->questions->skip(8)->take(1)),
+                        'step_4' => QuizQuestionResource::collection($this->questions->skip(9)->take(3)),
                         'step_5' => QuizQuestionResource::collection($this->questions->skip(15)->take(1)),
                         'step_6' => QuizQuestionResource::collection($this->questions->skip(16)->take(4)),
                         'step_7' => QuizQuestionResource::collection($this->questions->skip(20)->take(1)),
                         'step_8' => QuizQuestionResource::collection($this->questions->skip(21)->take(4)),
+                    ],
+                ];
+            }
+
+            if ($consultaionTypeId == 3) {
+                return [
+                    'id' => $this->id,
+                    'title' => $this->name,
+                    'description' => $this->description,
+                    'steps' => [
+                        'step_1' => QuizQuestionResource::collection($this->questions->take(20)),
+
                     ],
                 ];
             }
