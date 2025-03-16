2c9564a7115dfd839fb48afdc461373eb32a8aef
Omar-A-Mahran9 <omar.a.m.mahran@gmail.com>
2025-03-16 04:16:37 +0200

    firat commit

--- app/Http/Resources/Api/QuizResource.php
+++ app/Http/Resources/Api/QuizResource.php
@@ -13,9 +13,27 @@ class QuizResource extends JsonResource
 // Get the consultaion_type_id from the request and convert it to an integer
 $consultaionTypeId = (int) $request->input('consultaion_type_id');
 
- 
+
             // Apply different logic if consultaion_type_id == 1
             if ($consultaionTypeId == 1) {
+                return [
+                    'id' => $this->id,
+                    'title' => $this->name,
+                    'description' => $this->description,
+                    'steps' => [
+                        'step_1' => QuizQuestionResource::collection($this->questions->take(5)),
+                        'step_2' => QuizQuestionResource::collection($this->questions->skip(5)->take(3)),
+                        'step_3' => QuizQuestionResource::collection($this->questions->skip(8)->take(6)),
+                        'step_4' => QuizQuestionResource::collection($this->questions->skip(14)->take(1)),
+                        'step_5' => QuizQuestionResource::collection($this->questions->skip(15)->take(1)),
+                        'step_6' => QuizQuestionResource::collection($this->questions->skip(16)->take(4)),
+                        'step_7' => QuizQuestionResource::collection($this->questions->skip(20)->take(1)),
+                        'step_8' => QuizQuestionResource::collection($this->questions->skip(21)->take(4)),
+                    ],
+                ];
+            }
+
+            if ($consultaionTypeId == 2) {
                 return [
                     'id' => $this->id,
                     'title' => $this->name,
