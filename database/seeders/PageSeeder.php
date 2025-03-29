cce0961f2dfeb7da1faefd859c1c6f17cafac519
Omar-A-Mahran9 <omar.a.m.mahran@gmail.com>
2025-03-29 12:42:51 +0200

    firat commit

---
+++ database/seeders/PageSeeder.php
@@ -0,0 +1,184 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Page;
+use Illuminate\Database\Seeder;
+
+class PageSeeder extends Seeder
+{
+    public function run()
+    {
+        // First Page: Home Page (Update if exists)
+        $page1 = Page::updateOrCreate(
+            ['title' => 'Home Page'],
+            ['title' => 'Home Page']
+        );
+
+        $section1 = $page1->sections()->updateOrCreate(
+            ['title' => 'عنوانك هنا تسليط
+                            الضوء على ما هو مهم'],
+            [
+                'description' => 'لوريم إيبسوم هو ببساطة نص وهمي من صناعة الطباعة والتنضيد. لقد
+                كان لوريم إيبسوم هو النص الوهمي القياسي في هذه الصناعة منذ عام
+                1500.',
+                'image' => 'about.jpg'
+            ]
+        );
+
+        $section2 = $page1->sections()->updateOrCreate(
+            ['title' => 'لماذا تعتبر الصحة العقلية مهمة'],
+            [
+                'description' => 'لوريم إيبسوم هو ببساطة نص وهمي من صناعة الطباعة والتنضيد، وكان لوريمإيبسوم هو الصناعة.',
+                'image' => 'services.jpg'
+            ]
+        );
+        $section2->items()->updateOrCreate(
+            ['title' => 'فائدة 1'],
+            ['description' => 'لوريم إيبسوم هو ببساطة نص
+                وهمي للطباعة والتنضيد', 'image' => 'web_dev.jpg']
+        );
+        $section2->items()->updateOrCreate(
+            ['title' => 'فائدة 1'],
+            ['description' => 'لوريم إيبسوم هو ببساطة نص
+                وهمي للطباعة والتنضيد', 'image' => 'web_dev.jpg']
+        );
+        $section2->items()->updateOrCreate(
+            ['title' => 'فائدة 1'],
+            ['description' => 'لوريم إيبسوم هو ببساطة نص
+                وهمي للطباعة والتنضيد', 'image' => 'web_dev.jpg']
+        );
+
+        $section3 = $page1->sections()->updateOrCreate(
+            ['title' => 'عنوانك هنا تسليط
+                            الضوء على ما هو مهم'],
+            [
+                'description' => 'لوريم إيبسوم هو ببساطة نص وهمي من صناعة الطباعة والتنضيد. لقد
+                كان لوريم إيبسوم هو النص الوهمي القياسي في هذه الصناعة منذ عام
+                1500.',
+                'image' => 'about.jpg'
+            ]
+        );
+        $section4 = $page1->sections()->updateOrCreate(
+            ['title' => 'عنوانك هنا تسليط
+                            الضوء على ما هو مهم'],
+            [
+                'description' => 'لوريم إيبسوم هو ببساطة نص وهمي من صناعة الطباعة والتنضيد. لقد
+                كان لوريم إيبسوم هو النص الوهمي القياسي في هذه الصناعة منذ عام
+                1500.',
+                'image' => 'about.jpg'
+            ]
+        );
+
+        $section5 = $page1->sections()->updateOrCreate(
+            ['title' => 'لماذا تعتبر الصحة العقلية مهمة'],
+            [
+                'description' => 'لوريم إيبسوم هو ببساطة نص وهمي من صناعة الطباعة والتنضيد، وكان لوريمإيبسوم هو الصناعة.',
+                'image' => 'services.jpg'
+            ]
+        );
+        $section5->items()->updateOrCreate(
+            ['title' => 'المنفعة الثانوية I'],
+            ['description' => 'لوريم إيبسوم هو ببساطة نص
+                وهمي للطباعة والتنضيد', 'image' => 'web_dev.jpg']
+        );
+        $section5->items()->updateOrCreate(
+            ['title' => 'المنفعة الثانوية I'],
+            ['description' => 'لوريم إيبسوم هو ببساطة نص
+                وهمي للطباعة والتنضيد', 'image' => 'web_dev.jpg']
+        );
+        $section5->items()->updateOrCreate(
+            ['title' => 'المنفعة الثانوية I'],
+            ['description' => 'لوريم إيبسوم هو ببساطة نص
+                وهمي للطباعة والتنضيد', 'image' => 'web_dev.jpg']
+        );
+
+        $section6 = $page1->sections()->updateOrCreate(
+            ['title' => 'لماذا تعتبر الصحة العقلية مهمة'],
+            [
+                'description' => 'لوريم إيبسوم هو ببساطة نص وهمي من صناعة الطباعة والتنضيد، وكان لوريمإيبسوم هو الصناعة.',
+                'image' => 'services.jpg'
+            ]
+        );
+        $section6->items()->updateOrCreate(
+            ['title' => 'المنفعة الثانوية I'],
+            ['description' => 'لوريم إيبسوم هو ببساطة نص
+                وهمي للطباعة والتنضيد', 'image' => 'web_dev.jpg']
+        );
+        $section6->items()->updateOrCreate(
+            ['title' => 'المنفعة الثانوية I'],
+            ['description' => 'لوريم إيبسوم هو ببساطة نص
+                وهمي للطباعة والتنضيد', 'image' => 'web_dev.jpg']
+        );
+        $section6->items()->updateOrCreate(
+            ['title' => 'المنفعة الثانوية I'],
+            ['description' => 'لوريم إيبسوم هو ببساطة نص
+                وهمي للطباعة والتنضيد', 'image' => 'web_dev.jpg']
+        );
+
+
+        $section7 = $page1->sections()->updateOrCreate(
+            ['title' => 'عنوانك هنا تسليط
+                            الضوء على ما هو مهم'],
+            [
+                'description' => 'لوريم إيبسوم هو ببساطة نص وهمي من صناعة الطباعة والتنضيد. لقد
+                كان لوريم إيبسوم هو النص الوهمي القياسي في هذه الصناعة منذ عام
+                1500.',
+                'image' => 'about.jpg'
+            ]
+        );
+
+
+        // Second Page: Contact Page (Update if exists)
+        $page2 = Page::updateOrCreate(
+            ['title' => 'About Page'],
+            ['title' => 'About Page']
+        );
+
+        $section7 = $page2->sections()->updateOrCreate(
+            ['title' => 'Get in Touch'],
+            [
+                'description' => 'Contact us for inquiries',
+                'image' => 'contact.jpg'
+            ]
+        );
+
+        $section8 = $page1->sections()->updateOrCreate(
+            ['title' => 'لماذا تعتبر الصحة العقلية مهمة'],
+            [
+                'description' => 'لوريم إيبسوم هو ببساطة نص وهمي من صناعة الطباعة والتنضيد، وكان لوريمإيبسوم هو الصناعة.',
+                'image' => 'services.jpg'
+            ]
+        );
+        $section8->items()->updateOrCreate(
+            ['title' => 'فائدة 1'],
+            ['description' => 'لوريم إيبسوم هو ببساطة نص
+                وهمي للطباعة والتنضيد', 'image' => 'web_dev.jpg']
+        );
+        $section8->items()->updateOrCreate(
+            ['title' => 'فائدة 1'],
+            ['description' => 'لوريم إيبسوم هو ببساطة نص
+                وهمي للطباعة والتنضيد', 'image' => 'web_dev.jpg']
+        );
+        $section8->items()->updateOrCreate(
+            ['title' => 'فائدة 1'],
+            ['description' => 'لوريم إيبسوم هو ببساطة نص
+                وهمي للطباعة والتنضيد', 'image' => 'web_dev.jpg']
+        );
+        $section9 = $page2->sections()->updateOrCreate(
+            ['title' => 'Get in Touch'],
+            [
+                'description' => 'Contact us for inquiries',
+                'image' => 'contact.jpg'
+            ]
+        );
+        $section109 = $page2->sections()->updateOrCreate(
+            ['title' => 'Get in Touch'],
+            [
+                'description' => 'Contact us for inquiries',
+                'image' => 'contact.jpg'
+            ]
+        );
+
+    }
+}
